4a2bb4411fcb76ef9fff70bb728f384b3582efac
Kurtis Kemple <kurtiskemple@gmail.com>
2015-01-17 11:54:28 -0500

    Generated documentation for 35116c64bd584c62da16780b200e5b58b167f28b

--- assets/behavior.js
+++ assets/behavior.js
@@ -1156,8 +1156,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "type": "heading",
               "data": {
                 "level": 3,
-                "title": "Overview",
-                "slug": "overview"
+                "title": "Talk Overview",
+                "slug": "talk-overview"
               },
               "depth": 3
             }, {
@@ -1179,8 +1179,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 3,
-            "title": "Overview",
-            "slug": "overview"
+            "title": "Talk Overview",
+            "slug": "talk-overview"
           },
           "depth": 3
         }, {
